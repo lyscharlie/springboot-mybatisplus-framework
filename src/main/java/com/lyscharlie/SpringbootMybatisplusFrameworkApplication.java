2a8e95a3d3ebe1903410311dc6bdc0215eba0c9e
lycharlie <lyscharlie@hotmail.com>
2019-07-04 11:17:05 +0800

    Add files via upload

---
+++ src/main/java/com/lyscharlie/SpringbootMybatisplusFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.lyscharlie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisplusFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMybatisplusFrameworkApplication.class, args);
+	}
+
+}

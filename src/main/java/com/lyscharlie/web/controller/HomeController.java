f08fe42009c5e96db1cd2b5b8a97e62fe32a265d
lyscharlie <lyscharlie@hotmail.com>
2019-07-06 19:14:35 +0800

    111

---
+++ src/main/java/com/lyscharlie/web/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.lyscharlie.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("index.htm")
+	public String index(){
+		return "index";
+	}
+}
